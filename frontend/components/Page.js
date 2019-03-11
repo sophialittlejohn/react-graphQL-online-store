9b3c32b1e60b9b214cb3d1938db7971258b170c4
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-03-11 16:08:20 +0100

    Inject global styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -24,6 +24,36 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+injectGlobal`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('./static/radnikanext-medium-webfont.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+  html {
+    box-sizing: border-box;
+    /* set base font size to 10 so all rems are base 10 */
+    font-size: 10px;
+  }
+  /* reset box sizing everywhere */
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black}
+  }
+`;
+
 class Page extends Component {
   render() {
     return (
