b23e5611d066c3262f64afdb8332c73e250cbb25
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-03-11 16:12:54 +0100

    Use provided nav styles

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,17 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
   <div>
     <Link href="/">
       <a>Home</a>
+  <NavStyles>
     </Link>
     <Link href="/sell">
       <a>Sell Something!</a>
     </Link>
   </div>
+  </NavStyles>
 );
 
 export default Nav;
