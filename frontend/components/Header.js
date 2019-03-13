1db756ab189d2d1c1b750a81bc0bb69e995ce662
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-03-13 16:38:03 +0100

    Add progress bar to route changes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,22 @@
-import Nav from './Nav';
-import Link from 'next/link';
+import React from 'react';
+import Router from 'next/router';
 import styled from 'styled-components';
+
+import Link from 'next/link';
+import Nav from './Nav';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
