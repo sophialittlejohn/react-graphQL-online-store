d8824b0d69cc3b4bece3a2ee4e5a1a4f01b149bb
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-03-15 11:38:58 +0100

    Remove console.log

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,6 @@
 const mutations = {
   async createItem(parent, args, context, info) {
     // TODO check if user is logged in
-    console.log(args);
     const item = await context.db.mutation.createItem(
       {
         data: { ...args }
