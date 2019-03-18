89a91672fce39115212f2b74894e7e7204df3e4c
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-03-18 10:37:17 +0100

    Configure app to work with apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,27 @@
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 import Page from '../components/Page';
 import { lightTheme } from '../components/styles/themes/lightTheme';
 import { darkTheme } from '../components/styles/themes/darkTheme';
+import { userInfo } from 'os';
 
 class MyApp extends App {
   state = {
     switched: false
   };
+
+  // this is needed because we're using next (server side rending)
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the userInfo
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
 
   toggleSwitch = () => {
     this.setState(prevState => {
@@ -18,19 +32,21 @@ class MyApp extends App {
   };
 
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page
-          theme={this.state.switched ? darkTheme : lightTheme}
-          onClick={this.toggleSwitch}
-        >
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page
+            theme={this.state.switched ? darkTheme : lightTheme}
+            onClick={this.toggleSwitch}
+          >
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
