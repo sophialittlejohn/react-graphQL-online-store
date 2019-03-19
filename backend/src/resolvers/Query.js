6588059066f369121c215cf6e785845b2b87101c
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-03-19 17:38:46 +0100

    Add singluar item query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,7 +4,8 @@ const Query = {
   //   async items(parent, args, context, info) {
   //     return context.db.query.items();
   //   }
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
 };
 
 module.exports = Query;
