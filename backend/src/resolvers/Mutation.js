93c28be2ecaa873aef3ebdb0382a9b9cfdffcfa1
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-04-03 14:52:18 +0200

    Destructure in resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -206,17 +206,17 @@ const mutations = {
       info
     );
   },
-  async addToCart(parent, args, context, info) {
+  async addToCart(parent, { id }, context, info) {
     // 1. make sure the user is signed in
     const { userId } = context.request;
     if (!userId) {
       throw new Error('You must be signed in');
     }
     // 2. Query the users current cart
-    const existingCartItem = await context.db.query.cartItems({
+    const [existingCartItem] = await context.db.query.cartItems({
       where: {
-        user: { ud: userId },
-        item: { id: args.id }
+        user: { id: userId },
+        item: { id }
       }
     });
     // 3. Check if that item is already in their cart and increment by 1 if it is
