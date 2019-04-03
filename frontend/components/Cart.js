29e02b1e4ae1ba5cbaa23a854fee838a3dc7ee07
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-04-03 15:26:27 +0200

    Display cart items to user

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -5,6 +5,10 @@ import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
+import User from './User';
+import CartItem from './CartItem';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import formatMoney from '../lib/formatMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -21,27 +25,43 @@ const TOGGLE_CART_MUTATION = gql`
 
 const Cart = () => {
   return (
-    <Mutation mutation={TOGGLE_CART_MUTATION}>
-      {toggleCart => (
-        <Query query={LOCAL_STATE_QUERY}>
-          {({ data }) => (
-            <CartStyles open={data.cartOpen}>
-              <header>
-                <CloseButton onClick={toggleCart} title="close">
-                  &times;
-                </CloseButton>
-                <Supreme>Your Cart</Supreme>
-                <p>You have __ items in your cart</p>
-              </header>
-              <footer>
-                <p>$10.10</p>
-                <SickButton>Checkout</SickButton>
-              </footer>
-            </CartStyles>
-          )}
-        </Query>
-      )}
-    </Mutation>
+    <User>
+      {({ data: { me } }) => {
+        if (!me) return null;
+        console.log(me);
+        return (
+          <Mutation mutation={TOGGLE_CART_MUTATION}>
+            {toggleCart => (
+              <Query query={LOCAL_STATE_QUERY}>
+                {({ data }) => (
+                  <CartStyles open={data.cartOpen}>
+                    <header>
+                      <CloseButton onClick={toggleCart} title="close">
+                        &times;
+                      </CloseButton>
+                      <Supreme>{me.name}'s Cart</Supreme>
+                      <p>
+                        You have {me.cart.length} item
+                        {me.cart.length === 1 ? '' : 's'} in your cart
+                      </p>
+                    </header>
+                    <ul>
+                      {me.cart.map(cartItem => (
+                        <CartItem key={cartItem.id} cartItem={cartItem} />
+                      ))}
+                    </ul>
+                    <footer>
+                      <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+                      <SickButton>Checkout</SickButton>
+                    </footer>
+                  </CartStyles>
+                )}
+              </Query>
+            )}
+          </Mutation>
+        );
+      }}
+    </User>
   );
 };
 
--- frontend/components/User.js
+++ frontend/components/User.js
@@ -11,6 +11,17 @@ const CURRENT_USER_QUERY = gql`
       email
       name
       permissions
+      cart {
+        id
+        quantity
+        item {
+          id
+          price
+          image
+          title
+          description
+        }
+      }
     }
   }
 `;
