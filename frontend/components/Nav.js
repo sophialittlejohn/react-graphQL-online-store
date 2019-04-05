70a3f34872803f5e5894099af77533df1af678ed
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-04-05 11:58:31 +0200

    Add cart count dot

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -5,6 +5,7 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
 import { TOGGLE_CART_MUTATION } from './Cart';
+import CartCount from './CartCount';
 
 const Nav = () => (
   <User>
@@ -26,7 +27,17 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
