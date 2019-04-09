3fbc5d893f9c11887574f92708825140f24e1364
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-04-09 11:41:39 +0200

    Use adopt to clean up render props mess

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -20,6 +20,7 @@ const CartItemStyles = styled.li`
 `;
 
 const CartItem = ({ cartItem }) => {
+  if (!cartItem.item) return <p>This item has been removed</p>;
   return (
     <CartItemStyles>
       <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
